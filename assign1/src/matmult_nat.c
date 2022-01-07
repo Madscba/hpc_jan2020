c8537a34dbb7f2da556d4c484b8958f9499555e4
Mads <s173934@student.dtu.dk>
2022-01-07 09:47:19 +0100

    new native version

--- assign1/src/matmult_nat.c
+++ assign1/src/matmult_nat.c
@@ -1,26 +1,4 @@
-
-
-/*int main()
-{
-	
-}
-
-double ** dmalloc_2d(int m, int n) {
-    if (m <= 0 || n <= 0) return NULL;
-    double **A = malloc(m * sizeof(double *));
-    if (A == NULL) return NULL;
-    A[0] = malloc(m*n*sizeof(double));
-    if (A[0] == NULL) {
-        free(A); 
-        return NULL; 
-    }
-    for (int i = 1; i < m; i++)
-        A[i] = A[0] + i * n;
-    return A;
-}*/
-
-
-
+//Matrix-Matrix multiplication algorithm with for-loop permutations
 
 void matmult_nat(int m,int n,int k,double **A,double **B,double **C){
 	
@@ -32,48 +10,23 @@ void matmult_nat(int m,int n,int k,double **A,double **B,double **C){
 			C[i][j] = 0;
 		}
 	}
-	
 	for(int i = 0; i< m; i++)
-	{
-		for(int j = 0; j < n; j++)
+	{	
+		for(int l = 0; l < k; l++)
 		{
-			for(int l = 0; l < k; l++){
-				C[i][j] += A[i][l] * B[l][j];
-			}
-		}
-	}
-	
-}
-
-
-void matmult_mnk(int m,int n,int k,double **A,double **B,double **C)
-{
-	//Same as native matmult func
-	//initialization of C
-	for(int i = 0; i< m; i++)
-	{
-		for(int j = 0; j < n; j++)
-		{
-			C[i][j] = 0;
-		}
-	}
-	
-	
-	for(int i = 0; i< m; i++)
-	{
-		for(int j = 0; j < n; j++)
-		{
-			C[i][j] = 0;
-			for(int l = 0; l < k; l++){
+			for(int j = 0; j < n; j++)
+			{
 				C[i][j] += A[i][l] * B[l][j];
 			}
 			
 		}
 	}
+	
 }
 
 void matmult_mkn(int m,int n,int k,double **A,double **B,double **C)
 {
+	//Same as native matmult func
 	//initialization of C
 	for(int i = 0; i< m; i++)
 	{
@@ -94,6 +47,32 @@ void matmult_mkn(int m,int n,int k,double **A,double **B,double **C)
 		}
 	}
 }
+
+void matmult_mnk(int m,int n,int k,double **A,double **B,double **C)
+{
+	//initialization of C
+	for(int i = 0; i< m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			C[i][j] = 0;
+		}
+	}
+	
+	
+	for(int i = 0; i< m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			C[i][j] = 0;
+			for(int l = 0; l < k; l++){
+				C[i][j] += A[i][l] * B[l][j];
+			}
+			
+		}
+	}
+}
+
 void matmult_nmk(int m,int n,int k,double **A,double **B,double **C)
 {
 	//initialization of C
