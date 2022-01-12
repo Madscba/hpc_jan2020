248aeff1e529c222d50755091d7caf6960d93f8a
s175549 <s175549@student.dtu.dk>
2022-01-12 15:54:28 +0100

    OMP_2

---
+++ assign2/OMP_2/jacobi.c
@@ -0,0 +1,27 @@
+/* jacobi.c - Poisson problem in 3d
+ * 
+ */
+#include <math.h>
+#include <float.h>
+
+void
+jacobi(double ***u, double ***u_old, double ***f, int N, double delta) {
+    int i, j, k;
+    for (i = 1; i < N+1; i++) 
+	{
+		for (j = 1; j < N+1; j++)   
+		{
+			for (k = 1; k < N+1; k++) 
+			{	
+				double tmpX = (u_old[i-1][j][k] + u_old[i+1][j][k]);
+				double tmpY = (u_old[i][j-1][k] + u_old[i][j+1][k]);
+				double tmpZ = (u_old[i][j][k-1] + u_old[i][j][k+1]);
+				u[i][j][k] = (tmpX + tmpY + tmpZ + delta*f[i][j][k]) / 6.0;
+			}
+		}
+	}
+    
+
+
+    // fill in your code here
+}
