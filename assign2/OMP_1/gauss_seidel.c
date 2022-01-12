5bf4f886fefb1b303d2af00857a9dae37c9c4a60
s175549 <s175549@student.dtu.dk>
2022-01-12 15:51:05 +0100

    Update gauss_seidel.c

--- assign2/OMP_1/gauss_seidel.c
+++ assign2/OMP_1/gauss_seidel.c
@@ -6,11 +6,14 @@
 void
 gauss_seidel(double ***u, double ***f, int N, int delta) 
 {
+	// This is a implementation of the wavefront parallelization scheme with doacross loops
     int i, j, k;
+	#pragma omp for schedule(static,1) ordered(2) private(i,j,k)
     for (i = 1; i < N+1; ++i) 
 	{
 		for (j = 1; j < N+1; ++j)   
 		{
+			#pragma omp ordered depend(sink: i-1,j) depend(sink: i,j-1)
 			for (k = 1; k < N+1; ++k) 
 			{	
 				double tmpX = (u[i-1][j][k] + u[i+1][j][k]);
@@ -18,6 +21,7 @@ gauss_seidel(double ***u, double ***f, int N, int delta)
 				double tmpZ = (u[i][j][k-1] + u[i][j][k+1]);
 				u[i][j][k] = (tmpX + tmpY + tmpZ + delta*f[i][j][k]) / 6.0;
 			}
+			#pragma omp ordered depend(source)
 		}
 	}
 }
