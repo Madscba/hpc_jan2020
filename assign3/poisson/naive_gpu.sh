8ccede281e8d3b9cf1752812af71f352952435da
s175549 <s175549@student.dtu.dk>
2022-01-19 20:21:25 +0100

    sh

--- assign3/poisson/naive_gpu.sh
+++ assign3/poisson/naive_gpu.sh
@@ -4,7 +4,7 @@
 
 #
 #BSUB -q hpcintrogpu
-#BSUB -J poisson_ref_cpu
+#BSUB -J poisson_ref_gpu_nat
 #BSUB -n 1
 #BSUB -R "rusage[mem=1024MB]"
 #BSUB -R "select[model=XeonE5_2650v4]"
@@ -14,32 +14,24 @@
 #BSUB -W 40
 ###BSUB -B 
 #BSUB -N 
-#BSUB -o O_ratio_1_%J.out 
-#BSUB -e E_ratio_1_%J.err 
+#BSUB -o O_gpu_nat_%J.out 
+#BSUB -e E_gpu_nat_%J.err 
 
 #CC=${1-"gcc"}
-NDIMS="256"
+NDIMS="4 8 16 32 64 128 256"
 START_T=16
 EXECUTABLE_J="../poisson_gpu"
-THREADS="1 2 4 6 8 10 12 14 16 18 20 22 24"
+THREADS="1"
 lscpu
 LOGEXT=$CC.dat
-export OMP_DISPLAY_ENV=verbose
-export OMP_DISPLAY_AFFINITY=TRUE
-export OMP_PLACES=cores
-export OMP_PROC_BIND=spread #close
 
 for NDIM in $NDIMS
 do
 	/bin/rm -f "./perf_j_$NDIM$LOGEXT"
-	for n in $THREADS
-	do
-	
-		export OMP_NUM_THREADS=$n
-		echo $EXECUTABLE_J  $NDIM 3000 0.0 $START_T 0 1 $n
-		$EXECUTABLE_J $NDIM 3000 0.0 $START_T 0 1  | grep -v CPU >> ./perf_numa_$NDIM$LOGEXT
+	export OMP_NUM_THREADS=$n
+	echo $EXECUTABLE_J  $NDIM 3000 0.0 $START_T 0 1 $n
+	$EXECUTABLE_J $NDIM 3000 0.0 $START_T 0 1  | grep -v CPU >> ./perf_numa_$NDIM$LOGEXT
 
-	done
 done
 
 exit 0
--- assign3/poisson/reference_omp/poisson_cpu_ref.sh
+++ assign3/poisson/reference_omp/poisson_cpu_ref.sh
@@ -3,28 +3,24 @@
 # Reference CPU solution - varying size
 
 ###				QUEUE?
-#BSUB -q hpcintrogpu
+#BSUB -q hpc
 #BSUB -J poisson_ref_cpu
 #BSUB -n 1
 #BSUB -R "rusage[mem=1024MB]"
-
-##				SHOULD  WHAT ABOUT GPU?
 #BSUB -R "select[model=XeonE5_2650v4]"
 #BSUB -R "span[hosts=1]"
-##				CAN I JUST CHANGE TO CPU?
-#BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -M 4GB
 #BSUB -W 40
 ###BSUB -B 
 #BSUB -N 
-#BSUB -o O_ratio_1_%J.out 
-#BSUB -e E_ratio_1_%J.err 
+#BSUB -o O_cpu_%J.out 
+#BSUB -e E_cpu_%J.err 
 
 #CC=${1-"gcc"}
-NDIMS="256"
+NDIMS="4 8 16 32 64 128 256"
 START_T=16
 EXECUTABLE_J="../poisson_j"
-THREADS="1 2 4 6 8 10 12 14 16 18 20 22 24"
+THREADS="1"
 lscpu
 LOGEXT=$CC.dat
 export OMP_DISPLAY_ENV=verbose
@@ -35,14 +31,10 @@ export OMP_PROC_BIND=spread #close
 for NDIM in $NDIMS
 do
 	/bin/rm -f "./perf_j_$NDIM$LOGEXT"
-	for n in $THREADS
-	do
-	
-		export OMP_NUM_THREADS=$n
-		echo $EXECUTABLE_J  $NDIM 3000 0.0 $START_T 0 1 $n
-		$EXECUTABLE_J $NDIM 3000 0.0 $START_T 0 1  | grep -v CPU >> ./perf_numa_$NDIM$LOGEXT
+	export OMP_NUM_THREADS=$n
+	echo $EXECUTABLE_J  $NDIM 3000 $START_T 0 1 $n
+	$EXECUTABLE_J $NDIM 3000 $START_T 0 1  | grep -v CPU >> ./cpu_$NDIM$LOGEXT
 
-	done
 done
 
 exit 0
