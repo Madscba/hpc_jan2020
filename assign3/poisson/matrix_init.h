1491a6d723277bc344c4dbdc103ffbdf6e223d9a
s175549 <s175549@student.dtu.dk>
2022-01-19 12:27:45 +0100

    Update matrix_init.h

--- assign3/poisson/matrix_init.h
+++ assign3/poisson/matrix_init.h
@@ -6,10 +6,4 @@ void init_f(int outer_size, double ***f);
 void init_bounds(int outer_size,double mw_T, double sw_T, double ***matrix);
 void init_inner(int outer_size,double start_T, double ***matrix);
 
-void init_f_analytical(int outer_size,  double ***f);
-double f_analytical(double x, double y, double z);
-void u_true_analytical(int outer_size,  double ***u);
-double u_analytical(double x, double y, double z);
-
-
 #endif
