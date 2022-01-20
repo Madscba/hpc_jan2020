ba429bcdb46aa6f4a6b92401668b35ac505ea9e6
s175549 <s175549@student.dtu.dk>
2022-01-20 15:15:28 +0100

    kernel

--- assign3/poisson/gpu_ex5/jacobi.cu
+++ assign3/poisson/gpu_ex5/jacobi.cu
@@ -37,7 +37,7 @@ jacobi(double ***u_d, double ***u_old_d, double ***f_d, double ***u_h, double **
 	while(k<iter_max)
     {
         // Execute kernel function
-        jacobi<<<1,1>>>(u_d,u_old_d,f_d,N,delta);
+        jacobi_kernel<<<1,1>>>(u_d,u_old_d,f_d,N,delta);
         checkCudaErrors(cudaDeviceSynchronize());
 		//  #Comment out when benchmarking!!#
         if ((k % 100) == 0)
