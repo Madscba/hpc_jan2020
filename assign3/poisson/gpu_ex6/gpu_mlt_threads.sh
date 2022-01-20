76c96b6eb528b2776e58db091dd9b007b9955d3d
s175549 <s175549@student.dtu.dk>
2022-01-20 10:28:31 +0100

    sh script

---
+++ assign3/poisson/gpu_ex6/gpu_mlt_threads.sh
@@ -0,0 +1,35 @@
+#!/bin/sh 
+
+# GPU solution with one threads per element in U - varying size
+
+#BSUB -J poisson_ref_gpu_mlt 
+#BSUB -q hpcintrogpu 
+#BSUB -n 1 
+#BSUB -R "span[hosts=1]"  
+#BSUB -R "select[model=XeonGold6126]"
+#BSUB -gpu "num=1:mode=exclusive_process"  
+#BSUB -W 10 
+#BSUB -R "rusage[mem=2048MB]"
+#BSUB -N 
+#BSUB -o O_gpu_nat_%J.out 
+#BSUB -e E_gpu_nat_%J.err  
+ 
+export TMPDIR=$__LSF_JOB_TMPDIR__ 
+module load cuda/11.5.1 
+ 
+export MFLOPS_MAX_IT=1 
+
+LOGEXT=$CC.dat
+NDIMS="4 8 16 32 64 128 256"
+EXECUTABLE_J="poisson_gpu"
+lscpu
+
+for NDIM in $NDIMS
+do
+	echo $EXECUTABLE_J $NDIM 10000 1 1
+	./$EXECUTABLE_J $NDIM 10000 1 1 | grep -v CPU >> ./gpu_$NDIM$LOGEXT
+
+done
+
+exit 0
+
