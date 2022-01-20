3a5e0105b329ba12bb48892dfacefff3e1e092ab
s175549 <s175549@student.dtu.dk>
2022-01-20 21:04:54 +0100

    frob

--- assign3/poisson/gpu_ex7/frobenius.cu
+++ assign3/poisson/gpu_ex7/frobenius.cu
@@ -1,7 +1,7 @@
 #include <math.h>
-double frobenius(double ***u, double ***u_old, int N){
+__global__
+void frobenius_kernel(double ***u, double ***u_old, int N,double sum){
     int i, j, k;
-    double sum=0.0;
     double dist;
     for (i = 1; i < N+1; i++) 
 	{
@@ -14,6 +14,23 @@ double frobenius(double ***u, double ***u_old, int N){
             }
         }
     }
-    return sqrt(sum);
-
 }
+
+double frobenius(double ***u, double ***u_old, int N){
+    int i, j, k;
+    double dist;
+    double sum = 0.0;
+    for (i = 1; i < N+1; i++) 
+	{
+		for (j = 1; j < N+1; j++)   
+		{
+			for (k = 1; k < N+1; k++) 
+			{   
+                dist = u[i][j][k] - u_old[i][j][k];
+                sum += dist*dist;
+            }
+        }
+    }
+    return sqrt(sum);
+}
+
