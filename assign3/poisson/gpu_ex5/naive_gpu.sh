5267b18e7b8b906337e94f43315e142b2c611e8e
s175549 <s175549@student.dtu.dk>
2022-01-20 10:03:46 +0100

    Update naive_gpu.sh

--- assign3/poisson/gpu_ex5/naive_gpu.sh
+++ assign3/poisson/gpu_ex5/naive_gpu.sh
@@ -8,7 +8,7 @@
 #BSUB -R "span[hosts=1]"  
 #BSUB -R "select[model=XeonGold6126]"
 #BSUB -gpu "num=1:mode=exclusive_process"  
-#BSUB -W 10 
+#BSUB -W 20 
 #BSUB -R "rusage[mem=2048]"
 #BSUB -N 
 #BSUB -o O_gpu_nat_%J.out 
@@ -19,15 +19,15 @@ module load cuda/11.5.1
  
 export MFLOPS_MAX_IT=1 
 
+LOGEXT=$CC.dat
 NDIMS="4 8 16 32 64 128 256"
-START_T=16
 EXECUTABLE_J="poisson_gpu"
 lscpu
 
 for NDIM in $NDIMS
 do
-	echo $EXECUTABLE_J $NDIM 300 1 1
-	./$EXECUTABLE_J $NDIM 300 1 1
+	echo $EXECUTABLE_J $NDIM 1000 1 1
+	./$EXECUTABLE_J $NDIM 1000 1 1 | grep -v CPU >> ./gpu_$NDIM$LOGEXT
 
 done
 
