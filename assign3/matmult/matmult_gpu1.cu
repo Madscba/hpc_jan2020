2df3f2836734933791b58c6fc283c052cafebd0d
Anton Baht <anton@baht.dk>
2022-01-20 09:58:51 +0100

    gpulib

---
+++ assign3/matmult/matmult_gpu1.cu
@@ -0,0 +1,54 @@
+
+extern "C" {
+__global__
+void kernel_gpu1(int m, int n, int k, double *A, double *B, double *C){
+
+	for(int i = 0; i< m; i++)
+	{	
+		for(int l = 0; l < k; l++)
+		{
+			for(int j = 0; j < n; j++)
+			{
+				C[i*n+j] += A[i*k+l] * B[l*n+j];
+			}
+			
+		}
+	}
+}
+void matmult_gpu1(int m, int n, int k, double *A_h, double *B_h, double *C_h) 
+   { 
+      // Allocate A_d, B_d, C_d and transfer data 
+      //Initialize variables
+      double *A_d, *B_d, *C_d;
+      int A_size = m*k*sizeof( double );
+      int B_size = k*n*sizeof( double );
+      int C_size = m*n*sizeof( double );
+        for(int i = 0; i< m; i++)
+        {
+          for(int j = 0; j < n; j++)
+          {
+            C_h[i*n+j] = 0;
+          }
+        }
+
+      // Allocate on device
+      cudaMalloc( (void**)&A_d, A_size );
+      cudaMalloc( (void**)&B_d, B_size );
+      cudaMalloc( (void**)&C_d, C_size );
+
+      //Copy to device from host
+      cudaMemcpy(A_d, A_h, A_size, cudaMemcpyHostToDevice);
+      cudaMemcpy(B_d, B_h, B_size, cudaMemcpyHostToDevice);
+      cudaMemcpy(C_d, C_h, C_size, cudaMemcpyHostToDevice);
+      // Launch kernel and synchronize 
+      kernel_gpu1<<<1,1>>>(m,n,k,A_d,B_d,C_d);
+      cudaDeviceSynchronize();
+
+      cudaMemcpy(C_h, C_d, C_size, cudaMemcpyDeviceToHost);
+      
+      // Free A_d, B_d, C_d
+      cudaFree(A_d);
+      cudaFree(B_d);
+      cudaFree(C_d);
+   } 
+}
---
+++ assign3/matmult/matmult_gpu2.cu
@@ -0,0 +1,51 @@
+#include <math.h>
+#define BLOCK_SIZE 256
+extern "C" {
+__global__ void kernel_gpu2(int m, int n, int k, double *A, double *B, double *C){
+		int ti = blockIdx.y * blockDim.y + threadIdx.y;
+		int tj = blockIdx.x * blockDim.x + threadIdx.x;
+      double tmp = 0;
+
+      if (ti < m && tj < n){
+         C[ti*n+tj] = 0;
+      for (int l = 0; l < k; l++)
+		{
+         tmp += A[ti*k+l] * B[l*n+tj];
+      }
+      }
+      C[ti*n+tj] = tmp;
+			
+}
+void matmult_gpu2(int m, int n, int k, double *A_h, double *B_h, double *C_h) 
+   { 
+      // Allocate A_d, B_d, C_d and transfer data 
+      //Initialize variables
+      double *A_d, *B_d, *C_d;
+      int A_size = m*k*sizeof( double );
+      int B_size = k*n*sizeof( double );
+      int C_size = m*n*sizeof( double );
+      // Allocate on device
+      cudaMalloc( (void**)&A_d, A_size );
+      cudaMalloc( (void**)&B_d, B_size );
+      cudaMalloc( (void**)&C_d, C_size );
+
+      //Copy to device from host
+      cudaMemcpy(A_d, A_h, A_size, cudaMemcpyHostToDevice);
+      cudaMemcpy(B_d, B_h, B_size, cudaMemcpyHostToDevice);
+      cudaMemcpy(C_d, C_h, C_size, cudaMemcpyHostToDevice);
+      
+      //Define block grid
+      dim3 block(BLOCK_SIZE,BLOCK_SIZE);
+      dim3 grid(n/BLOCK_SIZE, m/BLOCK_SIZE);
+      // Launch kernel and synchronize
+      kernel_gpu2<<<grid,block>>>(m,n,k,A_d,B_d,C_d);
+      cudaDeviceSynchronize();
+
+      cudaMemcpy(C_h, C_d, C_size, cudaMemcpyDeviceToHost);
+      
+      // Free A_d, B_d, C_d
+      cudaFree(A_d);
+      cudaFree(B_d);
+      cudaFree(C_d);
+   } 
+}
