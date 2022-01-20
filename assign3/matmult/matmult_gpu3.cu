d5e09626cc81dac060e4d62ad2b177fd90077c73
Anton Baht <anton@baht.dk>
2022-01-20 12:57:26 +0100

    Skubbeskube

--- assign3/matmult/matmult_gpu3.cu
+++ assign3/matmult/matmult_gpu3.cu
@@ -1,21 +1,26 @@
 extern "C" {
 #include <math.h>
 #include <stdio.h>
-__global__ void kernel_gpu2(int m, int n, int k, double *A, double *B, double *C){
+__global__ void kernel_gpu3(int m, int n, int k, double *A, double *B, double *C){
 		int ti = blockIdx.y * blockDim.y + threadIdx.y;
 		int tj = blockIdx.x * blockDim.x + threadIdx.x;
-      double tmp = 0;
+      double tmp1 = 0;
+      double tmp2 = 0;
+      double tmp_b;
       //printf("i = %d, j = %d \n",ti,tj);
-      if (ti < m && tj < n){
+      if (2*ti < m && tj < n){
          for (int l = 0; l < k; l++)
          {
-            tmp += A[ti*k + l] * B[l*n + tj];
+            tmp_b = B[l*n + tj];
+            tmp1 += A[2*ti*k + l] * tmp_b;
+            tmp2 += A[(2*ti+1)*k + l] * tmp_b;
          }
-         C[ti*n+tj] = tmp;
+         C[2*ti*n+tj] = tmp1;
+         C[(2*ti+1)*n+tj] = tmp2;
          }
 			
 }
-void matmult_gpu2(int m, int n, int k, double *A_h, double *B_h, double *C_h) 
+void matmult_gpu3(int m, int n, int k, double *A_h, double *B_h, double *C_h) 
    { 
       // Allocate A_d, B_d, C_d and transfer data 
       //Initialize variables
@@ -34,10 +39,10 @@ void matmult_gpu2(int m, int n, int k, double *A_h, double *B_h, double *C_h)
 
       const int BLOCK_SIZE = 16;
       //Define block grid
-      dim3 block(BLOCK_SIZE,BLOCK_SIZE);
+      dim3 block(BLOCK_SIZE,BLOCK_SIZE/2);
       dim3 grid(ceil((double) n/BLOCK_SIZE),  ceil((double) m/BLOCK_SIZE));
       // Launch kernel and synchronize
-      kernel_gpu2<<<grid,block>>>(m,n,k,A_d,B_d,C_d);
+      kernel_gpu3<<<grid,block>>>(m,n,k,A_d,B_d,C_d);
       cudaDeviceSynchronize();
 
       cudaMemcpy(C_h, C_d, C_size, cudaMemcpyDeviceToHost);
--- assign3/matmult/matmult_gpu4.cu
+++ assign3/matmult/matmult_gpu4.cu
@@ -1,21 +1,35 @@
 extern "C" {
 #include <math.h>
 #include <stdio.h>
-__global__ void kernel_gpu2(int m, int n, int k, double *A, double *B, double *C){
+__global__ void kernel_gpu4(int m, int n, int k, double *A, double *B, double *C){
 		int ti = blockIdx.y * blockDim.y + threadIdx.y;
 		int tj = blockIdx.x * blockDim.x + threadIdx.x;
-      double tmp = 0;
+      double tmp11 = 0;
+      double tmp12 = 0;
+      double tmp21 = 0;
+      double tmp22 = 0;
+      double tmp_a1, tmp_a2, tmp_b1, tmp_b2;
       //printf("i = %d, j = %d \n",ti,tj);
-      if (ti < m && tj < n){
+      if (2*ti < m && 2*tj < n){
          for (int l = 0; l < k; l++)
          {
-            tmp += A[ti*k + l] * B[l*n + tj];
+            tmp_a1 = A[2*ti*k + l];
+            tmp_a2 = A[(2*ti+1)*k + l];
+            tmp_b1 = B[l*n + 2*tj];
+            tmp_b2 = B[l*n + 2*tj+1];
+            tmp11 += tmp_a1 * tmp_b1;
+            tmp12 += tmp_a1 * tmp_b2;
+            tmp21 += tmp_a2 * tmp_b1;
+            tmp22 += tmp_a2 * tmp_b2;
          }
-         C[ti*n+tj] = tmp;
+         C[2*ti*n+tj*2] = tmp11;
+         C[2*ti*n+tj*2+1] = tmp12;
+         C[(2*ti+1)*n+tj*2] = tmp21;
+         C[(2*ti+1)*n+tj*2+1] = tmp22;
          }
 			
 }
-void matmult_gpu2(int m, int n, int k, double *A_h, double *B_h, double *C_h) 
+void matmult_gpu4(int m, int n, int k, double *A_h, double *B_h, double *C_h) 
    { 
       // Allocate A_d, B_d, C_d and transfer data 
       //Initialize variables
@@ -34,10 +48,10 @@ void matmult_gpu2(int m, int n, int k, double *A_h, double *B_h, double *C_h)
 
       const int BLOCK_SIZE = 16;
       //Define block grid
-      dim3 block(BLOCK_SIZE,BLOCK_SIZE);
+      dim3 block(BLOCK_SIZE/2,BLOCK_SIZE/2);
       dim3 grid(ceil((double) n/BLOCK_SIZE),  ceil((double) m/BLOCK_SIZE));
       // Launch kernel and synchronize
-      kernel_gpu2<<<grid,block>>>(m,n,k,A_d,B_d,C_d);
+      kernel_gpu4<<<grid,block>>>(m,n,k,A_d,B_d,C_d);
       cudaDeviceSynchronize();
 
       cudaMemcpy(C_h, C_d, C_size, cudaMemcpyDeviceToHost);
