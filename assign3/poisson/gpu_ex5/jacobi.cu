f39e864b52421b44194ee9b9767894bbf5046180
s175549 <s175549@student.dtu.dk>
2022-01-20 20:41:35 +0100

    Update jacobi.cu

--- assign3/poisson/gpu_ex5/jacobi.cu
+++ assign3/poisson/gpu_ex5/jacobi.cu
@@ -22,11 +22,11 @@ jacobi_kernel(double ***u, double ***u_old, double ***f, int N, double delta) {
 			for (k = 1; k < N+1; k++) 
 			{	
 				tmpi = (u_old[i-1][j][k] + u_old[i+1][j][k]);
-				printf("i-1 %f i+1 %f tmpi %f sum %f",i,u_old[i-1][j][k],u_old[i+1][j][k],tmpi,u_old[i-1][j][k]+u_old[i+1][j][k]);
+				printf("i-1 %f i+1 %f tmpi %f sum %f",u_old[i-1][j][k],u_old[i+1][j][k],tmpi,u_old[i-1][j][k]+u_old[i+1][j][k]);
 				tmpj = (u_old[i][j-1][k] + u_old[i][j+1][k]);
-				printf(" j-1: %f j+1: %f ",j,u_old[i][j-1][k],u_old[i][j+1][k]);
+				printf(" j-1: %f j+1: %f ",u_old[i][j-1][k],u_old[i][j+1][k]);
 				tmpk = (u_old[i][j][k-1] + u_old[i][j][k+1]);
-				printf(" k-1: %f k+1: %f \n",k,u_old[i][j][k-1],u_old[i][j][k+1]);
+				printf(" k-1: %f k+1: %f \n",u_old[i][j][k-1],u_old[i][j][k+1]);
 				u[i][j][k] = (tmpi + tmpj + tmpk + delta*f[i][j][k]) / 6.0;
 				printf("(%i,%i,%i) f: %f i %f j %f k %f u %f old %f \n",i,j,k,f[i][j][k],tmpi,tmpj,tmpk,u[i][j][k],u_old[i][j][k]);
 			}
