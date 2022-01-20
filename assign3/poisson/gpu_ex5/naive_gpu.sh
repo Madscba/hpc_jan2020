26ffe49e7c710367e92bb4415e5a876767dee9f4
s175549 <s175549@student.dtu.dk>
2022-01-20 10:04:54 +0100

    Update naive_gpu.sh

--- assign3/poisson/gpu_ex5/naive_gpu.sh
+++ assign3/poisson/gpu_ex5/naive_gpu.sh
@@ -26,8 +26,8 @@ lscpu
 
 for NDIM in $NDIMS
 do
-	echo $EXECUTABLE_J $NDIM 1000 1 1
-	./$EXECUTABLE_J $NDIM 1000 1 1 | grep -v CPU >> ./gpu_$NDIM$LOGEXT
+	echo $EXECUTABLE_J $NDIM 10000 1 1
+	./$EXECUTABLE_J $NDIM 10000 1 1 | grep -v CPU >> ./gpu_$NDIM$LOGEXT
 
 done
 
