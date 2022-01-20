09dc029d66556d9c894be96aba75dfd0ce62fb8d
s175549 <s175549@student.dtu.dk>
2022-01-20 20:56:59 +0100

    Update jacobi.c

--- assign2/Poisson3D/jacobi.c
+++ assign2/Poisson3D/jacobi.c
@@ -16,13 +16,13 @@ jacobi(double ***u, double ***u_old, double ***f, int N, double delta) {
 			for (k = 1; k < N+1; k++) 
 			{	
 				tmpi = (u_old[i-1][j][k] + u_old[i+1][j][k]);
-				printf("i-1: %f i+1: %f ",i,u_old[i-1][j][k],u_old[i+1][j][k]);
+				//printf("i-1: %f i+1: %f ",i,u_old[i-1][j][k],u_old[i+1][j][k]);
 				tmpj = (u_old[i][j-1][k] + u_old[i][j+1][k]);
-				printf(" j-1: %f j+1: %f ",j,u_old[i][j-1][k],u_old[i][j+1][k]);
+				//printf(" j-1: %f j+1: %f ",j,u_old[i][j-1][k],u_old[i][j+1][k]);
 				tmpk = (u_old[i][j][k-1] + u_old[i][j][k+1]);
-				printf(" k-1: %f k+1: %f \n",k,u_old[i][j][k-1],u_old[i][j][k+1]);
+				//printf(" k-1: %f k+1: %f \n",k,u_old[i][j][k-1],u_old[i][j][k+1]);
 				u[i][j][k] = (tmpi + tmpj + tmpk + delta*f[i][j][k]) / 6.0;
-				printf("(%i,%i,%i) f: %f i %f j %f k %f u %f old %f \n",i,j,k,f[i][j][k],tmpi,tmpj,tmpk,u[i][j][k],u_old[i][j][k]);
+				//printf("(%i,%i,%i) f: %f i %f j %f k %f u %f old %f \n",i,j,k,f[i][j][k],tmpi,tmpj,tmpk,u[i][j][k],u_old[i][j][k]);
 			}
 		}
 	}   
