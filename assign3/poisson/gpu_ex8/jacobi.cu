7117a78df0b0a8ad7bedac217e3498f356557a5f
s175549 <s175549@student.dtu.dk>
2022-01-21 14:38:48 +0100

    ex 8

--- assign3/poisson/gpu_ex8/jacobi.cu
+++ assign3/poisson/gpu_ex8/jacobi.cu
@@ -12,23 +12,29 @@
 __global__ 
 void
 jacobi_reduction_baseline(double ***u, double ***u_old, double ***f, int N, double delta, double *d) {
-    double *a;
-	i = blockIdx.z * blockDim.z + threadIdx.z+1;
-	j = blockIdx.y * blockDim.y + threadIdx.y+1;
-	k = blockIdx.x * blockDim.x + threadIdx.x+1; 
+	double *a;
+	int i = blockIdx.z * blockDim.z + threadIdx.z+1;
+	int j = blockIdx.y * blockDim.y + threadIdx.y+1;
+	int k = blockIdx.x * blockDim.x + threadIdx.x+1; 
     if (i < N+1 && j < N+1 && k < N+1) {
-		tmpi = (u_old[i-1][j][k] + u_old[i+1][j][k]);
-		tmpj = (u_old[i][j-1][k] + u_old[i][j+1][k]);
-		tmpk = (u_old[i][j][k-1] + u_old[i][j][k+1]);
+		double tmpi = (u_old[i-1][j][k] + u_old[i+1][j][k]);
+		double tmpj = (u_old[i][j-1][k] + u_old[i][j+1][k]);
+		double tmpk = (u_old[i][j][k-1] + u_old[i][j][k+1]);
 		u[i][j][k] = (tmpi + tmpj + tmpk + delta*f[i][j][k]) / 6.0;
 		a = sqrt((u[i][j][k]-u_old[i][j][k])*(u[i][j][k]-u_old[i][j][k]));
-        atomicAdd(d,a);
+        atomicAdd(d,&a);
     }
 }
 
+__inline__ __device__ 
+double warpReduceSum(double value) { 
+    for (int i = 16; i > 0; i /= 2) 
+        value += __shfl_down_sync(-1, value, i);  
+    return value; 
+}
 
 __inline__ __device__ 
-double blockReduceSum(double*** value) { 
+double blockReduceSum(double value) { 
     __shared__ double smem[32]; // Max 32 warp sums 
  
     if (threadIdx.x < warpSize) 
@@ -46,24 +52,17 @@ double blockReduceSum(double*** value) {
     return warpReduceSum(value); 
 }
 
-__inline__ __device__ 
-double warpReduceSum(double value) { 
-    for (int i = 16; i > 0; i /= 2) 
-        value += __shfl_down_sync(-1, value, i);  
-    return value; 
-}
-
 __global__ 
 void jacobi_reduction_warp(double ***u, double ***u_old, double ***f, int N, double delta, double *d) 
 { 
 	double value;
-    i = blockIdx.z * blockDim.z + threadIdx.z+1;
-	j = blockIdx.y * blockDim.y + threadIdx.y+1;
-	k = blockIdx.x * blockDim.x + threadIdx.x+1;
+    int i = blockIdx.z * blockDim.z + threadIdx.z+1;
+	int j = blockIdx.y * blockDim.y + threadIdx.y+1;
+	int k = blockIdx.x * blockDim.x + threadIdx.x+1;
 	if (i < N+1 && j < N+1 && k < N+1) {
-		tmpi = (u_old[i-1][j][k] + u_old[i+1][j][k]);
-		tmpj = (u_old[i][j-1][k] + u_old[i][j+1][k]);
-		tmpk = (u_old[i][j][k-1] + u_old[i][j][k+1]);
+		double tmpi = (u_old[i-1][j][k] + u_old[i+1][j][k]);
+		double tmpj = (u_old[i][j-1][k] + u_old[i][j+1][k]);
+		double tmpk = (u_old[i][j][k-1] + u_old[i][j][k+1]);
 		u[i][j][k] = (tmpi + tmpj + tmpk + delta*f[i][j][k]) / 6.0;
 		value = sqrt((u[i][j][k]-u_old[i][j][k])*(u[i][j][k]-u_old[i][j][k]));
 	} else {
@@ -79,16 +78,16 @@ __global__
 void jacobi_reduction_presum(double ***u, double ***u_old, double ***f, int N, double delta, double *d) 
 { 
 	double value = 0.0;
-	idx_i = blockIdx.z * blockDim.z + threadIdx.z+1;
-	idx_j = blockIdx.y * blockDim.y + threadIdx.y+1;
-	idx_k = blockIdx.x * blockDim.x + threadIdx.x+1; 
+	int idx_i = blockIdx.z * blockDim.z + threadIdx.z+1;
+	int idx_j = blockIdx.y * blockDim.y + threadIdx.y+1;
+	int idx_k = blockIdx.x * blockDim.x + threadIdx.x+1; 
     //double value = 0; 
     for (int i = idx_i; i < N+1; i += blockDim.z * gridDim.z){
 		for (int j = idx_j; j < N+1; i += blockDim.y * gridDim.y){
 			for (int k = idx_; k < N+1; i += blockDim.x * gridDim.x){
-				tmpi = (u_old[i-1][j][k] + u_old[i+1][j][k]);
-				tmpj = (u_old[i][j-1][k] + u_old[i][j+1][k]);
-				tmpk = (u_old[i][j][k-1] + u_old[i][j][k+1]);
+				double tmpi = (u_old[i-1][j][k] + u_old[i+1][j][k]);
+				double tmpj = (u_old[i][j-1][k] + u_old[i][j+1][k]);
+				double tmpk = (u_old[i][j][k-1] + u_old[i][j][k+1]);
 				u[i][j][k] = (tmpi + tmpj + tmpk + delta*f[i][j][k]) / 6.0;
 				value += sqrt((u[i][j][k]-u_old[i][j][k])*(u[i][j][k]-u_old[i][j][k]));
 			}
@@ -122,14 +121,14 @@ int
 jacobi(double ***u_d, double ***u_old_d, double ***f_d, double ***u_h, double ***u_old_h, double ***f_h, int N, double delta, int iter_max, int NUM_BLOCKS, int THREADS_PER_BLOCK) {
 	double*** temp;
 	int k = 0;
-    double d = 10000000.0;
+    double *d = 10000000.0;
 	double tolerance = 1.0; 
 	dim3 dimBlock(10,10,10); //Threads per block
     dim3 dimGrid((N+dimBlock.x-1)/dimBlock.x,(N+dimBlock.y-1)/dimBlock.y,1); // Block in grid
 	while(d>tolerance && k<iter_max)
     {
         // Execute kernel function
-		jacobi_reduction_baseline<<<dimGrid,dimBlock>>>(u_d,u_old_d,f_d,N,delta);        
+		jacobi_reduction_baseline<<<dimGrid,dimBlock>>>(u_d,u_old_d,f_d,N,delta,d);        
 		checkCudaErrors(cudaDeviceSynchronize());
 		//  #Comment out when benchmarking!!#
         if ((k % 100) == 0)
--- assign3/poisson/gpu_ex8/jacobi.h
+++ assign3/poisson/gpu_ex8/jacobi.h
@@ -12,7 +12,7 @@ double warpReduceSum(double value);
 __global__ 
 void jacobi_reduction_warp(double ***u, double ***u_old, double ***f, int N, double delta, double *d);
 __global__ 
-void jacobi_reduction_presum(double ***u, double ***u_old, double ***f, int N, double delta, double *d) 
+void jacobi_reduction_presum(double ***u, double ***u_old, double ***f, int N, double delta, double *d); 
 __global__
 void jacobi_kernel(double ***u, double ***u_old, double ***f, int N, double delta);
 int jacobi(double ***u_d, double ***u_old_d, double ***f_d, double ***u_h, double ***u_old_h, double ***f_h, int N, double delta, int iter_max, int NUM_BLOCKS, int THREADS_PER_BLOCK);
